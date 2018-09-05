e7ab19abbd92222dea748f7a13475031ba77d03e
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-04 22:07:30 -0700

    Code moved from other project.

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,18 +1,14 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for Dictionary.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionary.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def word_list do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
   end
+
+  def random_word do
+    word_list()
+    |> Enum.random()
+  end
+  
 end
