8c3d7664faa0d7bd710ad27b99ea9b3ceddaeaf2
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-04 22:21:29 -0700

    Fix path issue.

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,7 +1,8 @@
 defmodule Dictionary do
 
   def word_list do
-    "assets/words.txt"
+    "../assets/words.txt"
+    |> Path.expand(__DIR__)
     |> File.read!()
     |> String.split(~r/\n/)
   end
